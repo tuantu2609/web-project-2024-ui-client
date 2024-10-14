7e9d2649976e6fe56adfc1a794104273fa8a83b8
tuantu2609 <tuantums10@gmail.com>
2024-10-14 22:48:18 +0700

    Create header, create page LearningPages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import LearningPages from "./pages/LearningPages";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<LearningPages />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
