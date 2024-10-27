0161f4ce2bffa8c969a829b38b002e4f27ea332a
WINDOWS-F6D8G21\Thông <21522644@gm.uit.edu.vn>
2024-10-27 22:17:34 +0700

    Login-Regis UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import LearningPages from "./pages/LearningPages";
 import LoginFrom from "./pages/test";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LearningPages />} />
-          <Route path="/login" element={<LoginFrom />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
         </Routes>
       </Router>
     </div>
