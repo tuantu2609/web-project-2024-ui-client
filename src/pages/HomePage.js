c6de88e0e036caea8b865f6d943f4b07e5077273
tuantu2609 <tuantums10@gmail.com>
2024-12-13 21:46:58 +0700

    Update alert message for CI/CD test deployment

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -32,7 +32,7 @@ const HomePage = ({ username }) => {
   };
 
   useEffect(() => {
-    alert("CI/CD Check: This is a third test deployment!");
+    alert("CI/CD Check: This is ci/cd test deployment!");
 
     setBodySectionMarginTop();
     fetch(`${API_URL}/courses`)
